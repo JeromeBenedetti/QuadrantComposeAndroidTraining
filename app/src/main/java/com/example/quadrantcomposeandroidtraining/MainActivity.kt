abe65e940691d52a25359216e4d43dc87adf21e1
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-07-01 02:36:06 +0200

    feat: create base quadrant

--- app/src/main/java/com/example/quadrantcomposeandroidtraining/MainActivity.kt
+++ app/src/main/java/com/example/quadrantcomposeandroidtraining/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quadrantcomposeandroidtraining.ui.theme.QuadrantComposeAndroidTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,10 +32,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun Quadrant(title: String, content: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+    ) {
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+        )
+        Text(
+            text = content,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.fillMaxWidth(),
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun QuadrantPreview() {
     QuadrantComposeAndroidTrainingTheme {
+        Quadrant("Lorem", "Lorem ipsum si dolor amet")
     }
 }
